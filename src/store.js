b4ddb55e6def1fe2d3b7c2666f5c9dffcda0d084
Satyam kumar <satyamkumar2716@gmail.com>
2026-02-22 02:24:16 +0530

    store change

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { api } from './services/api';
-
+//store.js
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
