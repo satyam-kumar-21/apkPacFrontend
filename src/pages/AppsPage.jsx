57e84b5d8ba4e4560c6a749d12b59725bd0d59b6
Satyam kumar <satyamkumar2716@gmail.com>
2026-03-03 05:57:59 +0530

    home routes showing same as apps routes

--- src/pages/AppsPage.jsx
+++ src/pages/AppsPage.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import TopFreeAppsList from "../components/TopFreeAppsList";
 import AdsSection from "../components/AdsSection";
 import PopularGamesSection from "../components/PopularGamesSection";
+import PopularAppsSection from "../components/PopularAppsSection";
 import PopularDesktopSoftwares from "../components/PopularDesktopSoftwares";
 import FinanceAppsSection from "../components/FinanceAppsSection";
 import EntertainmentAppsSection from "../components/EntertainmentAppsSection";
+import CommunicationAppsSection from "../components/CommunicationAppsSection";
 import ToolsAppsSection from "../components/ToolsAppsSection";
+import ShoppingAppsSection from "../components/ShoppingAppsSection";
+import ProductivityAppsSection from "../components/ProductivityAppsSection";
+import BusinessAppsSection from "../components/BusinessAppsSection";
+import SocialAppsSection from "../components/SocialAppsSection";
 
 const AppsPage = () => {
   return (
@@ -14,27 +20,63 @@ const AppsPage = () => {
       <div className="my-8">
         <AdsSection />
       </div>
+
       <TopFreeAppsList />
       <div className="my-8">
         <AdsSection />
       </div>
+
       <PopularGamesSection />
       <div className="my-8">
         <AdsSection />
       </div>
+
+      <PopularAppsSection />
+      <div className="my-8">
+        <AdsSection />
+      </div>
+
       <PopularDesktopSoftwares />
       <div className="my-8">
         <AdsSection />
       </div>
+
       <FinanceAppsSection />
       <div className="my-8">
         <AdsSection />
       </div>
+
       <EntertainmentAppsSection />
       <div className="my-8">
         <AdsSection />
       </div>
+
+      <CommunicationAppsSection />
+      <div className="my-8">
+        <AdsSection />
+      </div>
+
       <ToolsAppsSection />
+      <div className="my-8">
+        <AdsSection />
+      </div>
+
+      <ShoppingAppsSection />
+      <div className="my-8">
+        <AdsSection />
+      </div>
+
+      <ProductivityAppsSection />
+      <div className="my-8">
+        <AdsSection />
+      </div>
+
+      <BusinessAppsSection />
+      <div className="my-8">
+        <AdsSection />
+      </div>
+
+      <SocialAppsSection />
       <div className="my-8">
         <AdsSection />
       </div>
